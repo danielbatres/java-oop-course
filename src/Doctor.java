ccf1edb25a093576b1b35d901b3f116d41b99b8e
danielbatres <danielbatresuser@gmail.com>
2022-07-08 21:33:38 -0600

    Creando nuestra primera Clase

---
+++ src/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    // Atributos
+    int id;
+    String name;
+    String speciality;
+
+    // Comportamientos, métodos
+    public void showName() {
+        System.out.println(name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declaración e instanciación
+        Doctor myDoctor = new Doctor();
+
+        // Asignación de valor al atributo name
+        myDoctor.name = "Daniel";
+
+        // Uso de método del objeto
+        myDoctor.showName();
+    }
+}
