0ffe7de2fbc3bfcdccfca2b310644103d31dd3d2
danielbatres <danielbatresuser@gmail.com>
2022-07-09 11:30:40 -0600

    Final: Variables Constantes

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,8 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Declaración e instanciación
-        Doctor myDoctor = new Doctor();
-
-        // Asignación de valor al atributo name
-        myDoctor.name = "Daniel";
-
-        // Uso de método del objeto
-        myDoctor.showName();
-        myDoctor.showId();
-
-        Doctor.id++;
-
-        Doctor myDoctorAnn = new Doctor();
-        myDoctor.showId();
 
         showMenu();
+
     }
 }
