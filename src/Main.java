fe66e26fcee47295b330c15c869a8c55a74593af
danielbatres <danielbatresuser@gmail.com>
2022-07-09 18:18:56 -0600

    Variable vs. Objeto: Un vistazo a la memoria

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,26 @@ public class Main {
         System.out.println(myDoctor.speciality);
 
         int i = 0;
+        int b = 2;
+        b = i; // b = 0
         String name = "Daniel";
 
         Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        Patient patient2 = new Patient("Anahi", "anahi@mail.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
 
         patient.setWeight(54.5);
         System.out.println(patient.getWeight());
+
+        patient2.setName("Manuel"); // también afectara a patient
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
 
         patient.setPhoneNumber("12345678");
         System.out.println(patient.getPhoneNumber());
